d5ac54f6bcb4b0aab1100e8d1f3d2432c4a57970
Emma Böcker <57072067+StckOverflw@users.noreply.github.com>
2022-05-05 14:37:03 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("plugin.serialization") version "1.6.20"
     application
 }
